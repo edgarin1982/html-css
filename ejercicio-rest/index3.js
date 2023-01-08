4e8ba80072f947a63293d9da79fb4c574be4874e
edgarin <maed2802@gmail.com>
2023-01-07 21:28:30 -0600

    switch

---
+++ ejercicio-rest/index3.js
@@ -0,0 +1,24 @@
+const color = prompt("Ingrese un color");
+
+switch (color) {
+    case "rojo" :
+        alert("Es rojo") 
+        
+        break;
+    case "amarillo" :
+        alert("Es amarillo") 
+        
+        break;
+    case "negro" :
+        alert("Es negro") 
+        
+        break;
+    case "dorado" :
+        alert("Es dorado") 
+        
+        break;
+
+    default:
+        alert('Ninguna coincidencia')
+        break;
+}
