665788f9a73a0e1e5eadf9fab8d41f4fb91c56a4
edgarin <maed2802@gmail.com>
2023-01-07 20:58:46 -0600

    fin

--- ejercicio-rest/index.js
+++ ejercicio-rest/index.js
@@ -3,3 +3,7 @@ const totalBebida = 900 + 650;
 
 
 const total = totalComida + totalBebida;
+ 
+document.write("el total de comida es: " + totalComida);
+console.log("el total de bebida es: " + totalBebida);
+console.log("El total de la cuenta es: " + total);
